2fe96fcc017f1627a81bed332c15ac880fbc4207
Soluckyo <twinki0000@yandex.ru>
2026-06-20 21:19:02 +0300

    feat: реализовал и внедрил запись в таблицу логов аномалий

--- app/loader/anomaly_loader.py
+++ app/loader/anomaly_loader.py
@@ -11,26 +11,28 @@ def log_anomaly(
         severity: str,
         record: dict,
         message: str,
+        field_name: Optional[str] = None,
+        expected_value: Optional[str] = None,
         raw_id: int = None,
         stg_id: int = None
 ) -> int:
     conn = get_connection
 
-    coin_id = record.get("id")
+    coin_id = record.get("coin_id")
     symbol = record.get("symbol")
-    
+    actual_value = record.get(field_name)
 
     try:
         with conn.cursor() as cursor:
 
             sql = """
-                    INSERT INTO public.anomaly_log(anomaly_type, severity, coin_id, symbol, field_name, expected_value, actual_value, raw_id, stg_id, raw_record, message)
-                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                    INSERT INTO public.anomaly_log(anomaly_type, severity, coin_id, symbol, field_name, expected_value, actual_value, raw_id, stg_id, message)
+                    VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
                     RETURNING id;
                 """
             
             cursor.execute(sql,
-                           anomaly_type, severity, )
+                           anomaly_type, severity, coin_id, symbol, field_name, expected_value, actual_value, raw_id, stg_id, message)
             conn.commit()
             inserted_id = cursor.fetchone()[0]
 
