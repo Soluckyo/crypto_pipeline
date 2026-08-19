31e4101d741fdfeb79d455033e51d634b8ed3d68
Soluckyo <161376800+Soluckyo@users.noreply.github.com>
2026-08-20 00:15:46 +0300

    Dev (#9)
    * feat: добавил создание dim/fact таблиц
    
    * feat: добавил таблицу dim_date
    
    * wip: начал функцию генерации дат и сохранению в бд
    
    * feat: добавлен загрузчик dim таблиц
    
    * feat: добавил загрузчик fact таблицы
    
    * feat: добавлен dwh слой
    
    * Feature/mart layer (#6)
    
    * feat: добавил dbt и 3 витрины
    
    * fix: исправление ошибок с витринами
    
    * feat: dbt
    
    * Squashed commit of the following:
    
    commit cc5fdf6bac96bb5425e5413b70b9682db234a738
    Author: Soluckyo <twinki0000@yandex.ru>
    Date:   Thu Aug 20 00:03:59 2026 +0300
    
        feat: add metabase
    
    commit 363a5b3a4bc5d1e5609009a0aa60a045fcd3a433
    Author: Soluckyo <twinki0000@yandex.ru>
    Date:   Wed Aug 19 23:18:30 2026 +0300
    
        fix
    
    * Feature/bi (#8)
    
    * fix
    
    * feat: add metabase

---
+++ mart/target/compiled/mart/models/mart/market_overview.sql
@@ -0,0 +1,23 @@
+
+
+WITH daily_agg AS (
+    SELECT 
+        id_date,
+        COUNT(DISTINCT id_cryptocurrency) as active_coins,
+        SUM(market_cap) as total_market_cap,
+        SUM(volume_24h) as total_volume,
+        AVG(price_usd) as avg_price
+    FROM "crypto_pipeline"."dwh"."fact_market_snapshot"
+    GROUP BY id_date
+)
+SELECT 
+    d.full_date,
+    a.active_coins,
+    a.total_market_cap,
+    a.total_volume,
+    a.avg_price
+FROM daily_agg a
+JOIN "crypto_pipeline"."dwh"."dim_date" d 
+    ON a.id_date = d.id_date
+ORDER BY d.full_date DESC
+LIMIT 30
